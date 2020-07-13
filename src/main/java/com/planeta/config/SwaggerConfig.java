a61962428f10a1d795a21612a435f4cdbbe89a5a
paulo gomes <paulochavesgomes2@gmail.com>
2020-07-13 00:35:24 -0300

    make it better

--- src/main/java/com/planeta/config/SwaggerConfig.java
+++ src/main/java/com/planeta/config/SwaggerConfig.java
@@ -3,8 +3,10 @@ package com.planeta.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -17,13 +19,24 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class SwaggerConfig {
 	
+	
 	@Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-          .select()
-          .apis(RequestHandlerSelectors.any())
-          .paths(PathSelectors.any())
-          .build();
-    }		
+	public Docket api() {
+	    return new Docket(DocumentationType.SWAGGER_2)
+	        .select()
+	        .apis(RequestHandlerSelectors.basePackage("com.planeta"))
+	        .paths(PathSelectors.any())
+	        .build()	        
+	        .apiInfo(apiInfo());
+	}
+
+	private ApiInfo apiInfo() {
+	    return new ApiInfoBuilder()
+	            .title("API GUERA NAS ESTRELAS")
+	            .description("Desenvolvido por Paulo Chaves Gomes\n"
+	            		+ "")	            	         
+	            .build();
+	}
+
 
 }
