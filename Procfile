49c87a23c7d3f497bb5436204c1d0cb25eecb36c
paulo gomes <paulochavesgomes2@gmail.com>
2020-07-13 12:50:20 -0300

    make it better

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java -Dserver.port=$PORT -Dspring.profiles.ative=prod -jar target/planeta-0.0.1-SNAPSHOT.jar
--- src/main/java/com/planeta/PlanetaApplication.java
+++ src/main/java/com/planeta/PlanetaApplication.java
@@ -3,6 +3,7 @@ package com.planeta;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class PlanetaApplication {
 
@@ -10,5 +11,7 @@ public class PlanetaApplication {
 		SpringApplication.run(PlanetaApplication.class, args);
 		
 	}
+	
+	
 
 }
